197cf1dd1f06185e7201f262e359e696df5ffd60
Paulo Ferreira <phgferreira@outlook.com>
2023-07-23 00:19:23 -0300

    Montado o Playground

---
+++ src/views/playground/PlaygroundIndex.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full h-3/4 flex flex-col justify-center items-center bg-brand-main">
+    <h1 class="text-6xl font-black text-brand-gray">Playground</h1>
+    <p class="text-2xl mt-3 font-regular text-brand-gray">Este é o Playground, use para testar o Widget.</p>
+  </div>
+  <div class="w-full h-3/4 flex flex-col justify-center items-center bg-brand-gray">
+    <h1 class="text-6xl font-black text-brand-gray-dark">Coloque seu Widget bem aqui</h1>
+    <widget-index></widget-index>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import WidgetIndex from '@/views/widget/WidgetIndex.vue'
+</script>
