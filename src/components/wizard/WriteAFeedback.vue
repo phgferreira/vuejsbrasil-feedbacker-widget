7fe91ebeb1a302792b49e80c94163368fd6aa572
Paulo Ferreira <phgferreira@outlook.com>
2023-07-24 21:26:44 -0300

    Formulário para escrever o feedback

--- src/components/wizard/WriteAFeedback.vue
+++ src/components/wizard/WriteAFeedback.vue
@@ -1,11 +1,84 @@
 <template>
-  <h1>Write a feedback</h1>
+  <div class="flex flex-col items-center justify-center w-full my-5">
+    <textarea v-model="state.feedback" />
+    <button
+      :disabled="submitButtonIsDisabled"
+      :class="{
+        'opacity-50': state.isLoading,
+        'opacity-50 bg-gray-100 text-gray-500': submitButtonIsDisabled,
+        'bg-brand-main text-white': !submitButtonIsDisabled
+      }"
+      @click="submitAFeedback"
+    >
+      <icon v-if="state.isLoading" name="loading" class="animate-spin" color="white" />
+      <template v-else>
+        Enviar feedback
+      </template>
+    </button>
+  </div>
 </template>
 
 <script setup lang="ts">
+import useStore from '@/hooks/store'
+import { computed, defineComponent, reactive } from 'vue'
+import useNavigation from '@/hooks/navigation'
+import Icon from '@/components/Icon/IconIndex.vue'
+import services from '@/services'
+import { setMessage } from '@/store'
 
+defineComponent({
+  components: { Icon }
+})
+
+type State = {
+  feedback: string,
+  isLoading: boolean,
+  hasError: Error | null
+}
+
+const store = useStore()
+const { setErrorState, setSuccessState } = useNavigation()
+const state = reactive<State>({
+  feedback: '',
+  isLoading: false,
+  hasError: null
+})
+const submitButtonIsDisabled = computed<boolean>(() => {
+  return !state.feedback.length
+})
+
+function handleError (error: Error): void {
+  state.hasError = error
+  state.isLoading = false
+  setErrorState()
+}
+
+async function submitAFeedback (): Promise<void> {
+  setMessage(state.feedback)
+  state.isLoading = true
+  try {
+    const response = await services.feedback.create({
+      type: store.feedbackType,
+      text: store.message,
+      page: store.currentPage,
+      apiKey: store.apiKey,
+      decide: window.navigator.userAgent,
+      fingerprint: state.fingerprint
+    })
+    if (!response.errors) setSuccessState()
+    else setErrorState()
+    state.isLoading = false
+  } catch (error) {
+    handleError(error)
+  }
+}
 </script>
 
-<style scoped lang="postcss">
-
+<style scoped>
+textarea {
+  @apply w-full rounded-xl border-2 border-gray-300 border-solid h-24 p-2 resize-none focus:outline-none;
+}
+button {
+  @apply rounded-lg font-black mt-3 flex flex-col justify-center items-center py-2 w-full cursor-pointer focus:outline-none transition-all duration-200;
+}
 </style>
--- src/hooks/navigation.ts
+++ src/hooks/navigation.ts
@@ -6,18 +6,31 @@ import WriteAFeedback from '@/components/wizard/WriteAFeedback.vue'
 export interface Navigation {
   next(): void;
   back(): void;
+  setErrorState(): void;
+  setSuccessState(): void;
 }
 
 export default function useNavigation (): Navigation {
   const store = useStore()
+
   function next (): void {
     if (store.currentComponent.name === SelectFeedbackType.name) setCurrentComponent(WriteAFeedback)
   }
+
   function back (): void {
     if (store.currentComponent.name === WriteAFeedback.name) {
       setCurrentComponent(SelectFeedbackType)
       setFeedbackType('')
     }
   }
-  return { next, back }
+
+  function setErrorState (): void {
+    setCurrentComponent('Error')
+  }
+
+  function setSuccessState (): void {
+    setCurrentComponent('Success')
+  }
+
+  return { next, back, setErrorState, setSuccessState }
 }
